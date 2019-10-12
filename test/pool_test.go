9be1d8c4e6a9b72b4155011bb25d7ce4e223d264
tungyao <1922527051@qq.com>
2019-10-12 16:04:33 +0800

    init

---
+++ test/pool_test.go
@@ -0,0 +1,75 @@
+package test
+
+import (
+	"../../tpool"
+	"fmt"
+	"testing"
+)
+
+func TestPool2(t *testing.T) {
+	n := 0
+	e := tpool.NewTask(func() error {
+		n++
+		fmt.Println(n)
+		return nil
+	})
+	p := tpool.NewPool(10)
+	go func() {
+
+		for {
+			p.EntryChannel <- e
+		}
+	}()
+
+	p.Run()
+}
+
+//func TestPool(t *testing.T) {
+//	var p tnpool.OPool
+//	p.Init(9, 90)
+//	for i := 0; i < 9; i++ {
+//		p.AddTask(func() error {
+//			time.Sleep(time.Second * 2)
+//			fmt.Println(i)
+//			return nil
+//		})
+//	}
+//	p.SetFinishCallback(func() {
+//		fmt.Println("输出完成")
+//	})
+//	p.Start()
+//	p.Stop()
+//}
+
+//func TestChan(t *testing.T) {
+//	ch := make(chan string)
+//	go func() {
+//		ch <- "send"
+//	}()
+//
+//}
+
+//func TestChan1(t *testing.T) {
+//	g := make(chan func())
+//	quit := make(chan bool)
+//
+//	go func() {
+//		for {
+//			select {
+//			case v := <-g:
+//				v()
+//			case <-quit:
+//				fmt.Println("B退出")
+//				return
+//			}
+//		}
+//	}()
+//
+//	for i := 0; i < 3; i++ {
+//		g <- func() {
+//			time.Sleep(time.Second*1)
+//		}
+//	}
+//	quit <- true
+//	fmt.Println("testAB退出")
+//}
